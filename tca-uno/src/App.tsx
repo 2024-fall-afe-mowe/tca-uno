6fdeaaea58e92a05bf4c5fee9e794689a6d722ee
PopcornProblems <126116250+PopcornProblems@users.noreply.github.com>
2024-10-07 11:06:08 -0500

    Play and setup
    Add play and set up to Uno

--- tca-uno/src/App.tsx
+++ tca-uno/src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
+import { Home } from "./Home";
+import { Setup } from "./Setup";
+import { Play } from "./Play";
+
+const myRouter = createHashRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/setup",
+      element: <Setup />,
+    },
+    {
+      path: "/play",
+      element: <Play />,
+    },    
+  ]
+);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App p-3"
+    >
+      <RouterProvider 
+        router={myRouter}
+      />
     </div>
   );
 }
